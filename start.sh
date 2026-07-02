2b01529473520311a744108069ba703af242c8aa
malek-a7med <ma5191668@gmail.com>
2026-07-02 23:44:09 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# تحميل ملف Paper 1.20.4 لو مش موجود
+# تحميل ملف Paper 1.20.4 باستخدام wget لو مش موجود
 if [ ! -f paper.jar ]; then
   echo "Downloading Paper Minecraft Server..."
-  curl -o paper.jar https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/497/downloads/paper-1.20.4-497.jar
+  wget -O paper.jar https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/497/downloads/paper-1.20.4-497.jar
 fi
 
 # تشغيل السيرفر بجافا
