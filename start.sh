155542746fdbf011c83ebb7ba464d5a723a188de
malek-a7med <ma5191668@gmail.com>
2026-07-02 23:48:51 +0300

    Refactor start.sh by removing unnecessary comments
    Removed comment about downloading Paper file and streamlined server execution.

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# تحميل ملف Paper 1.20.4 باستخدام wget لو مش موجود
 if [ ! -f paper.jar ]; then
   echo "Downloading Paper Minecraft Server..."
   wget -O paper.jar https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/497/downloads/paper-1.20.4-497.jar
 fi
-
-# تشغيل السيرفر بجافا
 exec java -Xms768M -Xmx768M -jar paper.jar nogui
